574a98ccf5039c175f8377fbcd2c66070d9da746
divyapolson <divyapolson@gmail.com>
2017-12-04 18:58:04 -0600

    added image upload

--- source/client/components/Card/Card.jsx
+++ source/client/components/Card/Card.jsx
@@ -8,16 +8,33 @@ class Card extends Component {
 		super(props)
 	}
 
+	//image upload (partially functional)
+	readURL(input) {
+  	if (input.files && input.files[0]) {
+    		var reader = new FileReader();
+				reader.onload = function (e) {
+    				$('#pic')
+        		.attr('src', e.target.result);
+    		};
+		reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+
 	render() {
 		return (
 			<div className = "Card">
 				<Form>
 			  	<Form.Field control={Input} label='Card title' placeholder='Calculus Tutor' />
 			    <Form.Field control={TextArea} label='Describe the card and any specific requirements you have.' placeholder='Example: I’m looking for someone with experience tutoring college students in advanced calculus and available weekly Monday nights.' />
-			    <Form.Group widths='equal'>
+					//add image upload input and preview
+					<input type='file' onchange="readURL(this);" />
+					<img id="pic" src="http://placehold.it/180" alt="your image" />
+					<Form.Group widths='equal'>
 			    		<Form.Field control={Input} label='Your task location' placeholder='Champaign, IL' />
 			       	<Form.Field control={Input} label='Date/Deadline' placeholder='ASAP' />
 			    </Form.Group>
+					//add tags functionality
 			    <Form.Field control={TextArea} label='Tags (select up to five)'/>
 			    <Form.Group inline>
 			        <Form.Field control={Button}>Offer</Form.Field>
