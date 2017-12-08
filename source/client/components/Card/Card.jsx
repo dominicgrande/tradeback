af42d1f0d07419987b0ad4e6c703f32d79c59866
divyapolson <divyapolson@gmail.com>
2017-12-07 20:22:48 -0600

    imported semantic ui css

--- source/client/components/Card/Card.jsx
+++ source/client/components/Card/Card.jsx
@@ -27,14 +27,12 @@ class Card extends Component {
 				<Form>
 			  	<Form.Field control={Input} label='Card title' placeholder='Calculus Tutor' />
 			    <Form.Field control={TextArea} label='Describe the card and any specific requirements you have.' placeholder='Example: I’m looking for someone with experience tutoring college students in advanced calculus and available weekly Monday nights.' />
-					//add image upload input and preview
-					<input type='file' onchange="readURL(this);" />
+					<input type='file' onChange="readURL(this);" />
 					<img id="pic" src="http://placehold.it/180" alt="your image" />
 					<Form.Group widths='equal'>
 			    		<Form.Field control={Input} label='Your task location' placeholder='Champaign, IL' />
 			       	<Form.Field control={Input} label='Date/Deadline' placeholder='ASAP' />
 			    </Form.Group>
-					//add tags functionality
 			    <Form.Field control={TextArea} label='Tags (select up to five)'/>
 			    <Form.Group inline>
 			        <Form.Field control={Button}>Offer</Form.Field>
