e630cf4e1ea03c7427a1ec5fe15186812ed960db
divyapolson <divyapolson@gmail.com>
2017-12-07 20:21:38 -0600

    added react router

--- source/client/index.jsx
+++ source/client/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {HashRouter as Router, Route, Link, hashHistory } from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css';
 
 import Home from './components/Home/Home.jsx'
+import Card from './components/Card/Card.jsx';
 
 render(
-	<Home />,
+	<div>
+	<Router history={hashHistory}>
+		<div>
+		<Route exact path='/' component={Home}/>,
+		<Route exact path='/card' component={Card}/>,
+		</div>
+	</Router>
+	</div>,
 	document.getElementById('app')
 );
